a84295a8b678623bd8ee812e3e13d5d0f082d118
Sebastiaan Kok <sebastiaan@linux.com>
2026-01-20 10:50:22 +0100

    feat: add restic backups for /data

--- modules/virtual/k3s-home/storage.nix
+++ modules/virtual/k3s-home/storage.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   systemd.services.dailyBackup = {
     description = "Daily backup of /data to /storage/backups";
@@ -11,11 +12,64 @@
   };
 
   systemd.timers.dailyBackup = {
-    description = "Run dailyBackup service every day at 06:00";
+    description = "Run dailyBackup service every day 6 hour interval";
     timerConfig = {
-      OnCalendar = "06:00";
+      #OnCalendar = "12:00";
+      OnCalendar = [ "06:00" "12:00" "18:00" ];
       Persistent = true;
     };
     wantedBy = [ "timers.target" ];
   };
+
+  services.restic = {
+    enable = true;
+
+    backups = {
+      "data-b2" = {
+        paths = [ "/data" ];
+
+        # Global exclude file
+        excludes = [
+          "/library"
+
+          # Logs
+          "/logs/*"
+          "home-assistant.log*"
+
+          "frigate/clips/*"
+          "frigate/frigate/*"
+          "frigate/model_cache/*"
+          "frigate/exports/*"
+
+          # Caches
+          "**/cache/**"
+          "**/metadata/**"
+
+          # SQLite journals globally
+          "**/logs.db"
+          "**/*.db-shm"
+          "**/*.db-wal"
+        ];
+
+        # B2 repository
+        repository = "s3:s3.eu-central-003.backblazeb2.com/nix-restic/backups/data";
+
+        # Secrets
+        passwordFile = config.sops.secrets."restic-repo-password".path;
+        environmentFile = config.sops.secrets."b2s3-config".path;
+
+        # Retention policy
+        pruneOpts = [
+          "--keep-daily" "7"
+          "--keep-weekly" "1"
+          "--keep-monthly" "1"
+          "--keep-yearly" "0"
+        ];
+
+        timerConfig.OnCalendar = "06:00";
+        timerConfig.Persistent = true;
+      };
+    };
+  };
+
 }
