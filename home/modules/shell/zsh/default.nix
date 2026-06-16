b52e70019f5cefb069b9ce3169912371696d2d93
Sebastiaan Kok <sebastiaan@linux.com>
2026-06-16 14:59:31 +0200

    chore: add aliasses

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -19,6 +19,10 @@
       ask = "aider --chat-mode ask";
       aider = "bash $HOME/projects/toolbox/llm/aider_wrapper.sh";
       sync = "rclone sync k3s-home:/data ~/backups/k3s-home --exclude 'library/**' --exclude 'frigate/frigate/**' --progress --links --transfers 16 --checkers 16";
+      vi = "nvim";
+      vim = "nvim";
+      diff = "colordiff -Naur";
+      vimdiff = "nvim -d";
     };
     zplug = {
       enable = true;
