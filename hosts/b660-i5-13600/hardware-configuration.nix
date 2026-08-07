9da467b4b4a5d4c5e0fd3907451602e2bb605e1b
Sebastiaan Kok <sebastiaan@linux.com>
2026-08-07 12:46:41 +0200

    refactor: replace vaapiVdpau with libva-vdpau-driver

--- hosts/b660-i5-13600/hardware-configuration.nix
+++ hosts/b660-i5-13600/hardware-configuration.nix
@@ -25,7 +25,7 @@
     extraPackages = with pkgs; [
       intel-media-driver
       intel-vaapi-driver
-      vaapiVdpau
+      libva-vdpau-driver
       intel-compute-runtime
       vpl-gpu-rt
     ];
